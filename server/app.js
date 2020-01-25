b4349b1470abebd5082a237c3d8a25c4aa3ce13f
jayantamgr <jayantamgr@gmail.com>
2020-01-25 13:27:20 +0100

    added server, schema, query functiosn for book and author

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema:schema
+}));
+
+app.listen(4000, () => {
+    console.log('running on port 4000')
+});
+
+
+
+
